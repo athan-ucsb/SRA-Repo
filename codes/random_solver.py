29778c12802acf66525c06bbd42d7b057b578e2e
TheSandwichCoder <athan@ucsb.edu>
2026-07-10 12:53:19 -0700

    found kl divergence
    {'RandomSolver': np.float64(0.16298833862672282), 'MetropolisSolver': np.float64(1.5373619085456505), 'GibbsSolver': np.float64(1.1211864541512315), 'LiftSolver': np.float64(1.4145030972393284)}

--- codes/random_solver.py
+++ codes/random_solver.py
@@ -4,48 +4,16 @@ from GLOBAL import _rng
 class RandomSolver(Solver):
     def __init__(self, g1, q, beta=1, n_seconds=None):
         super().__init__(g1, q, beta, n_seconds)
+        self.name = "random"
 
-    def solve(self):
-        solved = False
-        best_conflicts = self.g1.n_nodes * self.g1.n_nodes
+    def solve_single(self):
+        node_i = _rng.integers(0, self.g1.n_nodes)
+        node = self.g1.nodes[node_i]
 
-        time_list = []
-        n_conflicts_list = []
+        old_local_conflicts = self.g1.count_conflicts_i(node_i)
+        
+        node.color = _rng.integers(0, self.q)
 
-        self.profiler.start()
+        new_local_conflicts = self.g1.count_conflicts_i(node_i)
 
-        while not solved:
-            if self.n_seconds is not None and self.profiler.get_elapsed_time() > self.n_seconds:
-                if self.print_info:
-                    print("Time limit reached.")
-                break
-
-            for node in self.g1.nodes:
-                node.color = _rng.integers(0, self.q)
-            new_conflicts = self.g1.count_conflicts()
-
-            if new_conflicts < best_conflicts:
-                best_conflicts = new_conflicts
-
-                if self.print_info:
-                    print("Best conflicts: ", best_conflicts)
-
-            # only check every 1000 iterations
-            if self.profiler.past_interval():  # Update the second count
-                n_conflicts_list.append(best_conflicts)
-                time_list.append(self.profiler.get_elapsed_time())
-
-            if new_conflicts == 0:
-                solved = True
-
-
-        if self.save_plot:
-            _save_conflict_plot(
-                time_list,
-                n_conflicts_list,
-                'Conflicts vs Time - Random',
-                "stats/random_solver.png",
-            )
-
-
-        return self.g1
+        return new_local_conflicts - old_local_conflicts
